813eb8b4a1eb4e63a9896df9483b02134d01ba72
NathanFink <nathanfink@u.boisestate.edu>
2025-04-16 03:47:30 -0600

    Ballgame starter javascript

--- js/main.js
+++ js/main.js
@@ -1,64 +1,198 @@
-/**
- * This file should only contain functions that don't interact with the DOM.
- * That means no document.querySelector, no document.getElementById, etc.
- * This file should only contain functions that do things like calculations,
- * data manipulation, etc. This is so that we can test these functions
- * without having to worry about the DOM or the browser environment.
- */
+// set up canvas
 
-/**
- * This function prints the string 'Hello World' to the console
- */
-export function helloWorld() {
-    console.log('Hello World');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/**
- * This function adds two numbers together and returns the sum. This demonstrates
- * how to pass parameters to a function and return a value from a function
- * from HTML.
- * @param {number} a The first number to add
- * @param {number} b The second number to add
- * @returns {number} The sum of the two numbers
- */
-export function add(num1, num2) {
-    if (num1 === undefined || num2 === undefined) {
-        throw new Error('You must provide two numbers to add');
-    }
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        throw new Error('You must provide two numbers to add');
-    }
-    return num1 + num2;
+// function to generate random RGB color value
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-/**
- * This function fetches a random joke from the "Official Joke API" and returns it.
- * @returns {string} A joke in the format "setup - punchline"
- */
-export async function fetchRandomJoke() {
-    try {
-        const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-        if (!response.ok) {
-            throw new Error('Failed to fetch a joke');
+let ballCount = 0;
+const ballCountDisplay = document.getElementById("ballCount");
+
+function updateBallCount(){
+  ballCountDisplay.textContent = "Ball count: " + ballCount;
+}
+
+
+//added shape class
+class Shape {
+  constructor(x, y, velX, velY){
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
+//added evil cirlce class
+class EvilCircle extends Shape{
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'white';
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+      draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.lineWidth = 3;
+      }
+
+      checkBounds() {
+        if (this.x + this.size >= width) {
+          this.x = width - this.size;
         }
-        const joke = await response.json();
-        return `${joke.setup} - ${joke.punchline}`;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+    
+        if (this.x - this.size <= 0) {
+          this.x = this.size;
+        }
+    
+        if (this.y + this.size >= height) {
+          this.y = height - this.size;
+        }
+    
+        if (this.y - this.size <= 0) {
+          this.y = this.size;
+        }
+      }
+
+      collisionDetect() {
+        for (const ball of balls) {
+          if (ball.exists) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+    
+            if (distance < this.size + ball.size) {
+              ball.exists = false;
+              ballCount = ballCount - 1;
+              updateBallCount();
+            }
+          }
+        }
+      }
 }
 
-export async function fetch5RandomJokes() {
-    try {
-        // This endpoint returns 10 jokes, so we need to slice the array to get 5
-        const response = await fetch('https://official-joke-api.appspot.com/random_ten');
-        if (!response.ok) {
-            throw new Error('Failed to fetch a joke');
-        }
-        const jokes = await response.json();
-        // Slice the first 5 jokes from the array
-        return jokes.slice(0, 5).map(joke => `${joke.setup} - ${joke.punchline}`);
-    } catch (error) {
-        throw new Error(error.message);
+class Ball extends Shape{
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -Math.abs(this.velX);
     }
+
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball && ball.exists)) { //added the if exist is set to true for a specification
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  ballCount = ballCount + 1;
+  balls.push(ball);
+}
+
+updateBallCount();
+
+const evilCircle = new EvilCircle(width/2 , height/2);
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    if (ball.exists){
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+  }
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+
+  requestAnimationFrame(loop);
+}
+
+loop();
